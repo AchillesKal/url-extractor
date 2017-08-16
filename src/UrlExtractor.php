d89b06fa3e90bee5820ad13596b7a3580b58bcf4
Achilles Kaloeridis <achilleas90gr@gmail.com>
2017-08-16 20:42:35 +0300

    adds test & actual code

--- src/UrlExtractor.php
+++ src/UrlExtractor.php
@@ -4,8 +4,39 @@ namespace AchillesKal\UrlExtractor;
 
 class UrlExtractor
 {
-    public function __construct()
+    private $url;
+
+    public function __construct($url)
     {
-        echo "UrlExtractor initialized";
+        $this->url = $url;
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function extractUrl()
+    {
+        $this->removeScheme();
+        $this->removeSubdomain();
+
+        return  $this->url;
+    }
+
+    public function removeScheme()
+    {
+        $this->url = preg_replace("(^https?://)", "", $this->url );
+        return $this;
+    }
+
+    public function removeSubdomain()
+    {
+        $parts = explode(".", $this->url);
+        $host1 = $parts[count($parts)-2];
+        $host2 = $parts[count($parts)-1];
+
+        $this->url = $host1 . '.' .  $host2;
+        return $this;
     }
 }
--- tests/UrlExtractorTest.php
+++ tests/UrlExtractorTest.php
@@ -2,14 +2,57 @@
 
 namespace AchillesKal\UrlExtractorTests;
 
+use AchillesKal\UrlExtractor\UrlExtractor;
 use PHPUnit\Framework\TestCase;
 
 class UrlExtractorTest extends TestCase
 {
-    public function testPushAndPop()
+    public function testExtractUrl()
     {
+        $urls = [
+            'https://achilleskal.com',
+            'http://achilleskal.com',
+            'https://www.achilleskal.com',
+            'http://www.achilleskal.com',
+        ];
 
-        $this->assertEquals(0, 0);
-        $this->assertEquals(2, 2);
+        foreach($urls as $url){
+            $extractor = new UrlExtractor($url);
+            $result = $extractor->extractUrl();
+
+            $this->assertEquals('achilleskal.com', $result);
+        }
+    }
+
+    public function testRemoveScheme()
+    {
+        $urls = [
+            'https://achilleskal.com',
+            'http://achilleskal.com',
+        ];
+
+        foreach($urls as $url){
+            $extractor = new UrlExtractor($url);
+            $result = $extractor->removeScheme();
+
+            $this->assertEquals('achilleskal.com', $result->getUrl());
+        }
+    }
+
+    public function testRemoveSubdomain()
+    {
+        $urls = [
+            'https://www.achilleskal.com',
+            'https://web.achilleskal.com',
+            'http://test.real.achilleskal.com',
+            'http://wuba.test.real.achilleskal.com',
+        ];
+
+        foreach($urls as $url){
+            $extractor = new UrlExtractor($url);
+            $result = $extractor->removeSubdomain();
+
+            $this->assertEquals('achilleskal.com', $result->getUrl());
+        }
     }
 }
